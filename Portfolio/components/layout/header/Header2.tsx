5ad4af9310a2b1422f0cbd4415abfd7afc6c2a47
DuLa_1sT <dulajhansana1973@gmail.com>
2024-12-02 16:33:45 +0530

    New

--- Portfolio/components/layout/header/Header2.tsx
+++ Portfolio/components/layout/header/Header2.tsx
@@ -29,7 +29,13 @@ export default function Header2({ scroll, isMobileMenu, handleMobileMenu,isOffCa
 											</li>
 											<li className="nav-item">
 												<Link className="nav-link" href="#services">Services</Link>
-											
+											</li>
+											<li className="nav-item">
+												<Link className="nav-link" href="#portfolio">Portfolio</Link>
+											</li>
+											<li className="nav-item">
+												<Link className="nav-link" href="#contact">Contact</Link>
+											</li>
 										</ul>
 									</div>
 								</div>
