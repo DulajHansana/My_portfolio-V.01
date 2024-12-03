9c97628ee27a791e6c1af7d168de4d61720dc3fa
DuLa_1sT <dulajhansana1973@gmail.com>
2024-12-04 01:10:48 +0530

    Update Services

--- Portfolio/components/sections/Service2.tsx
+++ Portfolio/components/sections/Service2.tsx
@@ -15,7 +15,7 @@ export default function Service2() {
 										<svg className="text-primary-2 me-2" xmlns="http://www.w3.org/2000/svg" width={5} height={6} viewBox="0 0 5 6" fill="none">
 											<circle cx="2.5" cy={3} r="2.5" fill="#A8FF53" />
 										</svg>
-										<span className="text-linear-4 d-flex align-items-center"> Cooperation </span>
+										<span className="text-linear-4 d-flex align-items-center"> Creative </span>
 									</div>
 									<h3>
 										<span className="text-300" style={{ marginRight: '10px' }}>Tailored</span>
@@ -45,15 +45,6 @@ export default function Service2() {
 												</svg>
 												<h6 className="my-3 fw-medium">Database Management</h6>
 												<p className="fs-7 text-300 fw-regular">Designing and managing databases with SQL and NoSQL technologies such as <span className="text-secondary-2">MySQL</span>, <span className="text-secondary-2">PostgreSQL</span>, and <span className="text-secondary-2">MongoDB</span>.</p>
-											</div>
-										</div>
-										<div className="col-lg-4 col-md-6">
-											<div className="card-servies-2 rounded-2 h-100 hover-up">
-												<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
-													<path d="M12 2C15.866 2 19 5.13401 19 9C19 9.11351 18.9973 9.22639 18.992 9.33857C21.3265 10.16 23 12.3846 23 15C23 18.3137 20.3137 21 17 21H7C3.68629 21 1 18.3137 1 15C1 12.3846 2.67346 10.16 5.00804 9.33857C5.0027 9.22639 5 9.11351 5 9C5 5.13401 8.13401 2 12 2ZM12 4C9.23858 4 7 6.23858 7 9C7 9.08147 7.00193 9.16263 7.00578 9.24344L7.07662 10.7309L5.67183 11.2252C4.0844 11.7837 3 13.2889 3 15C3 17.2091 4.79086 19 7 19H17C19.2091 19 21 17.2091 21 15C21 12.79 19.21 11 17 11C15.233 11 13.7337 12.1457 13.2042 13.7347L11.3064 13.1021C12.1005 10.7185 14.35 9 17 9C17 6.23858 14.7614 4 12 4Z" fill="#1F1F24" />
-												</svg>
-												<h6 className="my-3 fw-medium">API Development</h6>
-												<p className="fs-7 text-300 fw-regular">Creating and integrating <span className="text-secondary-2">RESTful APIs</span> to enable smooth communication between front-end and back-end systems.</p>
 											</div>
 										</div>
 										<div className="col-lg-4 col-md-6">
