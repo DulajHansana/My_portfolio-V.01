d519f2dcc8d124aa3c4c0f16d80e9f078b7d1983
DuLa_1sT <dulajhansana1973@gmail.com>
2024-12-04 01:00:36 +0530

    Remove Cooparation

--- Portfolio/app/page.tsx
+++ Portfolio/app/page.tsx
@@ -1,6 +1,5 @@
 import Layout from "@/components/layout/Layout"
 import Contact2 from '@/components/sections/Contact2'
-import Coporation2 from "@/components/sections/Coporation2"
 import Education2 from "@/components/sections/Education2"
 import Experience2 from "@/components/sections/Experience2"
 import Home2 from "@/components/sections/Home2"
@@ -15,7 +14,6 @@ export default function HomePage2() {
 			<Layout headerStyle={2} footerStyle={2}>
 				<Home2 />
 				<Static2 />
-				<Coporation2 />
 				<Service2 />
 				<Experience2 />
 				<Education2 />
