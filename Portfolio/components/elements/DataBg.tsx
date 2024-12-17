06e76c2434f101944e1592b735c7affc7854f6cc
DuLa_1sT <dulajhansana1973@gmail.com>
2024-12-17 23:22:39 +0530

    Update

--- Portfolio/components/elements/DataBg.tsx
+++ Portfolio/components/elements/DataBg.tsx
@@ -1,16 +1,1 @@
-import { useEffect } from 'react'
 
-export default function DataBg() {
-	useEffect(() => {
-		const elements = document.querySelectorAll<HTMLElement>('[data-background]')
-
-		elements.forEach((element) => {
-			const backgroundUrl = element.getAttribute('data-background')
-			if (backgroundUrl) {
-				element.style.backgroundImage = `url(${backgroundUrl})`
-			}
-		})
-	}, [])
-
-	return null
-}
