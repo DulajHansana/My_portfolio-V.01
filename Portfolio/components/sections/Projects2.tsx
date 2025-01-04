15ce9e79e39b855a35c5d72a26925aa8e95f798e
DuLa_1sT <dulajhansana1973@gmail.com>
2025-01-04 23:40:32 +0530

    Add navigation buttons to Projects2 component for improved user interaction

--- Portfolio/components/sections/Projects2.tsx
+++ Portfolio/components/sections/Projects2.tsx
@@ -450,7 +450,11 @@ export default function Projects2() {
 											</SwiperSlide>
 										</div>
 									</Swiper>
-									43L13.1924 18.3638L11.7782 19.778L4 11.9999L11.7782 4.22168L13.1924 5.63589L7.82843 10.9999Z" fill="white" />
+									
+									<div className="position-absolute bottom-0 end-0 gap-2 pb-7 pe-5 d-none d-md-flex">
+										<div className="swiper-button-prev end-0 shadow position-relative">
+											<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
+												<path d="M7.82843 10.9999H20V12.9999H7.82843L13.1924 18.3638L11.7782 19.778L4 11.9999L11.7782 4.22168L13.1924 5.63589L7.82843 10.9999Z" fill="white" />
 											</svg>
 										</div>
 										<div className="swiper-button-next end-0 shadow position-relative">
