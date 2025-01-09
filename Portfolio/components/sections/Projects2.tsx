16aa6ca52e148f940ed5c2d72dd947a094e3723d
DuLa_1sT <161925626+DulajHansana@users.noreply.github.com>
2025-01-09 11:26:35 +0530

    Update Projects2 component with new project images

--- Portfolio/components/sections/Projects2.tsx
+++ Portfolio/components/sections/Projects2.tsx
@@ -334,12 +334,17 @@ export default function Projects2() {
                                                                 loop={true}
                                                                 className="swiper project-image-slider"
                                                             >
-                                                                <SwiperSlide>
-                                                                    <img className="w-100" src="assets/imgs/home-page-2/projects/27.gif" alt="Project Image 1" />
-                                                                </SwiperSlide>
+                                                                
+																	
 																<SwiperSlide>
-                                                                    <img className="w-100" src="assets/imgs/home-page-2/projects/26.png" alt="Project Image 1" />
-                                                                </SwiperSlide>					
+                                                                    <img className="w-100" src="assets/imgs/home-page-2/projects/33.jfif" alt="Project Image 1" />
+                                                                </SwiperSlide>	
+																<SwiperSlide>
+                                                                    <img className="w-100" src="assets/imgs/home-page-2/projects/34.jfif" alt="Project Image 1" />
+                                                                </SwiperSlide>	
+																<SwiperSlide>
+                                                                    <img className="w-100" src="assets/imgs/home-page-2/projects/32.png" alt="Project Image 1" />
+                                                                </SwiperSlide>				
                                                             </Swiper>
                                                             <div className="swiper-button-prev-project shadow position-relative"></div>
                                                             <div className="swiper-button-next-project shadow position-relative"></div>
